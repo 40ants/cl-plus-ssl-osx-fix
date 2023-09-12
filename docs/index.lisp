bd753752117e42d1d6c7efa3d3e0dc2309d7a588
Alexander Artemenko <svetlyak.40wt@gmail.com>
2023-09-12 18:18:58 +0300

    Ignore NOTE.

--- docs/index.lisp
+++ docs/index.lisp
@@ -38,6 +38,7 @@
                                    "HTTP"
                                    "TODO"
                                    "Unlicense"
+                                   "NOTE"
                                    "SSL"
                                    "CL+"
                                    "OSX"
